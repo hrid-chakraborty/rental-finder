75ef27db82f7c7b8069d6364ed88f425039e6b06
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-06-15 11:59:16 +0300

    Added ContainerComponent to Navbar Component

---
+++ app/components/ContainerComponent.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+interface ContainerComponentProps {
+    children: React.ReactNode;
+}
+
+const ContainerComponent: React.FC<ContainerComponentProps> = ({children}) => {
+  return (
+    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+        {children}
+    </div>
+  )
+}
+
+export default ContainerComponent
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import ContainerComponent from "../ContainerComponent";
+
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
         <div className="py-4 border-b">
-          
+          <ContainerComponent>
+
+          </ContainerComponent>
         </div>
     </div>
   )
