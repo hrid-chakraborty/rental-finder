aeb6573cf9ffc2083c8542830cae9453123b1195
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-06-15 12:20:40 +0300

    Added UserMenu Component to Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@
 import ContainerComponent from "../ContainerComponent";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return (
@@ -12,6 +13,7 @@ const Navbar = () => {
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
                         <Logo />
                         <Search />
+                        <UserMenu />
                     </div>
                 </ContainerComponent>
             </div>
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { AiOutlineMenu } from 'react-icons/ai'
+import { useState,useCallback } from 'react'
+
+const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value)=>!value)
+    },[])
+
+    return (
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                <div onClick={() => { }} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-md hover:bg-neutral-100 transition cursor-pointer">
+                    Rent your home
+                </div>
+                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border border-neutral-200 flex flex-row items-center gap-3 rounded-md cursor-pointer hover:shadow-md transition">
+                    <AiOutlineMenu />
+                    <div className='hidden md:block'>
+                         
+                    </div>
+                </div>
+            </div>
+
+            {isOpen && (
+                <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+                    <div className='flex flex-col cursor-pointer'>
+                        <>
+                            
+                        </>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default UserMenu
