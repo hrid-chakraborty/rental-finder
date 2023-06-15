b9f54530db2111f2043a49ebdef1b6364f0f9c96
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-06-15 11:52:07 +0300

    Created Navbar Component

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div className="py-4 border-b">
+          
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google"
 import './globals.css'
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata = {
   title: 'Rental Finder',
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
       </body>
     </html>
