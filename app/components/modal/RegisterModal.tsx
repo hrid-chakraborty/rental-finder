94427c30e67e777bde233efd94e4c39531be8538
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-06-17 10:40:16 +0300

    Moved hooks folder out of components folder

--- app/components/modal/RegisterModal.tsx
+++ app/components/modal/RegisterModal.tsx
@@ -5,7 +5,7 @@ import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import useRegisterModal from "../hooks/useRegisterModal";
+import useRegisterModal from "../../hooks/useRegisterModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,7 +4,7 @@ import { AiOutlineMenu } from "react-icons/ai";
 import { useState, useCallback } from "react";
 import Avatar from "./Avatar";
 import MenuItem from "./MenuItem";
-import useRegisterModal from "../hooks/useRegisterModal";
+import useRegisterModal from "../../hooks/useRegisterModal";
 
 const UserMenu = () => {
   const registerModal = useRegisterModal();
