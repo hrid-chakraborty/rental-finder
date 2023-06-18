a2ee8a076e31e2c14ede20bbf346c6550336518c
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-06-18 12:20:39 +0300

    Added Google and Github OAuth

--- app/components/modal/LoginModal.tsx
+++ app/components/modal/LoginModal.tsx
@@ -88,13 +88,13 @@ const LoginModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={()=>{}}
+        onClick={()=>signIn("google")}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={()=>{}}
+        onClick={()=>signIn("github")}
       />
       <div className="
         text-neutral-500
