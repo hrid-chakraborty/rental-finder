8e1c042dbd0ff1d048146269c0b9fbb30834f0aa
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-06-21 11:36:02 +0300

    Added a Map which works with CountrySelect

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -9,6 +9,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -48,6 +49,10 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+
+  const Map = useMemo(() => dynamic(() => import('../Map'), {
+    ssr: false
+  }),[location]);
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -124,6 +129,7 @@ const RentModal = () => {
           value={location} 
           onChange={(value) => setCustomValue('location', value)} 
         />
+        <Map center={location?.latlng} />
       </div>
     );
   }
