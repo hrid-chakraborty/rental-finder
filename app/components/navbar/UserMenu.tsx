150252692224c9eed3ab62363e3d9bf9c09ddbb7
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-06-21 21:09:22 +0300

    Added List Of Reservations under My Reservations

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -61,7 +61,7 @@ const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
               <>
               <MenuItem onClick={()=>router.push('/trips')} label="My Trips" />
               <MenuItem onClick={()=>{}} label="My Favorites" />
-              <MenuItem onClick={()=>{}} label="My Reservations" />
+              <MenuItem onClick={()=>router.push('/reservations')} label="My Reservations" />
               <MenuItem onClick={()=>{}} label="My Properties" />
               <MenuItem onClick={rentModal.onOpen} label="Rent My Home" />
               <hr />
---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Heading from "@/app/components/Heading";
+import ContainerComponent from "@/app/components/ContainerComponent";
+import ListingCard from "@/app/components/listings/ListingCard";
+import { Reservation, User } from "@prisma/client";
+
+interface ReservationsClientProps {
+  reservations: Reservation[],
+  currentUser?: User | null,
+}
+
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+  reservations,
+  currentUser
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState('');
+
+  const onCancel = useCallback((id: string) => {
+    setDeletingId(id);
+
+    axios.delete(`/api/reservations/${id}`)
+    .then(() => {
+      toast.success('Reservation cancelled');
+      router.refresh();
+    })
+    .catch(() => {
+      toast.error('Something went wrong.')
+    })
+    .finally(() => {
+      setDeletingId('');
+    })
+  }, [router]);
+
+  return (
+    <ContainerComponent>
+      <Heading
+        title="Reservations"
+        subtitle="Bookings on your properties"
+      />
+      <div 
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {reservations.map((reservation: any) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            disabled={deletingId === reservation.id}
+            actionLabel="Cancel guest reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </ContainerComponent>
+   );
+}
+ 
+export default ReservationsClient;
